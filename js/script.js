9f9b802190185e98e9ac40ab8870f14fb2e4a5a3
Maria König <maria.konig@yh.nackademin.se>
2024-03-20 22:52:38 +0100

    fixes product image ratio

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
             data.forEach(product => {
                 // Skapa produktkort
                 const productCard = document.createElement('div');
-                productCard.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'mb-4');
+                productCard.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'mb-4', "product-container");
                 productCard.innerHTML = `
                 <div class="card h-100">
                     <img class="card-img-top" src="${product.image}" alt="${product.title}">
