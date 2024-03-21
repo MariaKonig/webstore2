bff4c25644efab5c21d72f6c2d38603fedaa83c4
Josef <josef@boukdir.se>
2024-03-21 20:19:46 +0100

    rewritten function for validation

--- js/main.js
+++ js/main.js
@@ -1,54 +1,37 @@
-function cancel(event) {
-    event.preventDefault();
-    window.location.href = 'start.html';
+
+// Form validation. 
+function checkInput(userInput, rule) {
+    userInput.addEventListener('input', function(){
+        if(rule.test(userInput.value)) {
+            userInput.classList.remove('is-invalid')
+            userInput.classList.add('is-valid')
+        } else {
+            userInput.classList.remove('is-valid')
+            userInput.classList.add('is-invalid')
+        }
+    })
 }
 
-const namnBox = document.getElementById('namn')
+const nameInput = document.getElementById('namn')
 const validName = /^[a-zåäö]{2,}\s[a-zåäö]{2,}$/i
-namnBox.addEventListener('input', function() {
-    if (validName.test(namnBox.value)) {
-        namnBox.classList.remove('is-invalid')
-        namnBox.classList.add('is-valid')
-    } else {
-        namnBox.classList.remove('is-valid')
-        namnBox.classList.add('is-invalid')
-    }
-})
+checkInput(nameInput, validName)
 
-const telefonBox = document.getElementById('telefon')
+const phoneInput = document.getElementById('telefon')
 const validPhone = /^[\d()-]{10,}$/
-telefonBox.addEventListener('input', function() {
-    if (validPhone.test(telefonBox.value)) {
-        telefonBox.classList.remove('is-invalid')
-        telefonBox.classList.add('is-valid')
-    } else {
-        telefonBox.classList.remove('is-valid')
-        telefonBox.classList.add('is-invalid')
-    }
-})
+checkInput(phoneInput, validPhone)
 
-const emailBox = document.getElementById('email')
+const emailInput = document.getElementById('email')
 const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-emailBox.addEventListener('input', function() {
-    if (validEmail.test(emailBox.value)) {
-        emailBox.classList.remove('is-invalid')
-        emailBox.classList.add('is-valid')
-    } else {
-        emailBox.classList.remove('is-valid')
-        emailBox.classList.add('is-invalid')
-    }
+checkInput(emailInput, validEmail)
+
+const addressInput = document.getElementById('adress')
+const validAddress = /^[\w\s-é]{2,50}\s[\d]{5,5}\s[a-zåäö]{2,50}$/i
+checkInput(addressInput, validAddress)
+
+// Redirect when closing modal in order.
+const modalID = document.getElementById('successModal')
+modalID.addEventListener('hidden.bs.modal', function(){
+    location.replace("produkter.html")
 })
 
-const adressBox = document.getElementById('adress')
-const validAddress = /^[\w\s-]{2,50}\s[\d]{5,5}\s[a-zåäö]{2,50}$/i
-//const validAddress = /^[\w\s-]{2,50}\s(?:\d\s*){5}\s[a-zåäö]{2,50}$/i
-adressBox.addEventListener('input', function(){
-    if (validAddress.test(adressBox.value)) {
-        adressBox.classList.remove('is-invalid')
-        adressBox.classList.add('is-valid')
-    } else {
-        adressBox.classList.remove('is-valid')
-        adressBox.classList.add('is-invalid')
-    }
-})
-
+addEventListener('')
